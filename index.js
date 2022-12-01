e24b1c500aa3d917064f74340248dc2d7e6918c1
Kellymugambi <kellymgambi@.com>
2022-12-01 23:04:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+// Write your code h
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.textContent = "Kelly is the champion"
